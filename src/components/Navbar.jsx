361648fe0641b595a7c93cae454b36b53fd0c615
Gabriel Aldea D <gabriel.aldea@gmail.com>
2025-03-26 12:31:25 -0300

    navbar rendering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import { assets } from '../assets/assets';
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} className="-36" alt="" />
@@ -24,6 +25,40 @@ const Navbar = () => {
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700"></hr>{' '}
         </NavLink>
       </ul>
+      <div className="flex items-center gap-6">
+        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <div className="group relative">
+          <img src={assets.profile_icon} className="w5 cursor-pointer" alt="" />
+          <div className="group-hover:block hidden absolute dropdown-menu right-o pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+        <Link to="/cart" className="relative">
+          {' '}
+          <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
+          <p className="absolute right-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            10
+          </p>
+        </Link>
+        <img
+          onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          className="w-5 cursor-pointer sm:hidden"
+          alt=""
+        />
+      </div>
+
+      {/* Sidebar menu for small screens
+
+      <div
+        className={
+          "absolute to=0 right=0 bottom=0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}"
+        }
+      ></div> */}
     </div>
   );
 };
